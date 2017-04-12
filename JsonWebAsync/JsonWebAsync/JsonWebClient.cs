f6563845a58868e7bd4673fca1a4fc7454c2cfef
rrTong <rrtongue@gmail.com>
2017-04-12 01:39:58 -0700

    Add files via upload

---
+++ JsonWebAsync/JsonWebAsync/JsonWebClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Net;
+
+namespace JsonWebAsync
+{
+    public class JsonWebClient
+    {
+        public async Task<System.IO.TextReader> DoRequestAsync(WebRequest req)
+        {
+            var task = Task.Factory.FromAsync((cb, o) => ((HttpWebRequest)o).BeginGetResponse(cb, o), res => ((HttpWebRequest)res.AsyncState).EndGetResponse(res), req);
+            var result = await task;
+            var resp = result;
+            var stream = resp.GetResponseStream();
+            var sr = new System.IO.StreamReader(stream);
+            return sr;
+        }
+
+        public async Task<System.IO.TextReader> DoRequestAsync(string url)
+        {
+            HttpWebRequest req = HttpWebRequest.CreateHttp(url);
+            req.AllowReadStreamBuffering = true;
+            var tr = await DoRequestAsync(req);
+            return tr;
+        }
+    }
+}
